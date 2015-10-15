88ac4cd768f9d08a029449bf17d0234a3df66a27
Sanjay Nair <sanjayhnair@gmail.com>
2015-10-15 19:39:39 -0400

    Global align works with all scoring. Traceback is highlighted. Arrows can be toggled

--- main/main.controller.js
+++ main/main.controller.js
@@ -25,6 +25,10 @@
             $scope.scoreDeleteScore = -2;
             $scope.scoreInsertScore = -2;
 
+            $scope.blossum45MismatchScore = -5;
+
+            $scope.showArrows = true;
+
 
         }
 
@@ -44,13 +48,13 @@
                         type: 'score',
                         up: false,
                         left: false,
-                        diag: false
+                        diag: false,
+                        color: 'white'
                     };
                 }
             }
 
             // Insert sequence
-            // Insert zeroes initially
             var seq1 = 0,
                 seq2 = 0;
             for (var i = 0; i < $scope.matrix.length; i++) {
@@ -80,23 +84,23 @@
             };
 
             console.log($scope.matrix);
-            
+
             $scope.initSubmatrix();
 
 
         }
-        
-        
+
+
         //onClick the Check button
         $scope.check = function () {
             console.log("check");
         }
-        
+
         // onClick the Run button
-        $scope.runNW = function () {
-            
+        $scope.initNW = function () {
+
             $scope.initialize();
-            
+
             // Insert zeroes initially
             for (var i = 0; i < $scope.matrix.length; i++) {
                 for (var j = 0; j < $scope.matrix[i].length; j++) {
@@ -110,117 +114,170 @@
                     }
                 }
             }
-            
-            // change zeroes if alignment type is global
-            switch ($scope.alignmentMethod) {
 
-            case "local" || "dovetail" || "pattern":
 
-                break;
-            case "global":
+            // If it's global alignment, need to modify first col and row for del and inserts
+            if ($scope.alignmentMethod === "global") {
+
+                var mismatchScore;
+
+                switch ($scope.scoringMethod) {
+
+                case "editDistance":
+                    mismatchScore = $scope.edMismatchScore;
+                    break;
+
+                case "score":
+                    mismatchScore = $scope.scoreMismatchScore;
+                    break;
+
+                case "blosum45":
+                    mismatchScore = $scope.blossum45MismatchScore;
+                    break;
+
+                }
+
                 for (var i = 2; i < $scope.matrix.length; i++) {
-                    $scope.matrix[i][1].value = $scope.matrix[i - 1][1].value + $scope.edMismatchScore;
-                    //$scope.matrix[i][1].up = true;
+                    $scope.matrix[i][1].value = $scope.matrix[i - 1][1].value + mismatchScore;
                 }
 
                 for (var j = 2; j < $scope.matrix[2].length; j++) {
-                    $scope.matrix[1][j].value = $scope.matrix[1][j - 1].value + $scope.edMismatchScore;
-                    //$scope.matrix[1][j].left = true;
+                    $scope.matrix[1][j].value = $scope.matrix[1][j - 1].value + mismatchScore;
                 }
+            }
 
+
+            // determine what scoring method will be used
+            var mismatch, match, insert, del;
+            switch ($scope.scoringMethod) {
+
+            case "editDistance":
+                mismatch = $scope.edMismatchScore;
+                insert = $scope.edMismatchScore;
+                del = $scope.edMismatchScore;
+                match = $scope.edMatchScore;
 
                 break;
-            }            
-            
-            // determine what scoring method will be used
-            var mismatch,match,insert,del;
-            switch($scope.scoringMethod){
-            
-                case "editDistance":
-                    mismatch = $scope.edMismatchScore;
-                    insert = $scope.edMismatchScore;
-                    del = $scope.edMismatchScore;
-                    match = $scope.edMatchScore;
-                    
-                    break;
-                
-                case "score":
-                    mismatch = $scope.scoreMismatchScore;
-                    insert = $scope.scoreInsertScore;
-                    del = $scope.scoreDeleteScore;
-                    match = $scope.scoreMatchScore;
-                    break;
-                    
-                case "blosum45":
-                    
-                    break;
-                    
+
+            case "score":
+                mismatch = $scope.scoreMismatchScore;
+                insert = $scope.scoreInsertScore;
+                del = $scope.scoreDeleteScore;
+                match = $scope.scoreMatchScore;
+                break;
+
+            case "blosum45":
+
+                break;
+
                 // use Edit Distance is some error happens
-                default:
-                    mismatch = $scope.edMismatchScore;
-                    insert = $scope.edMismatchScore;
-                    del = $scope.edMismatchScore;
-                    match = $scope.edMatchScore;
-                    break;
-            
+            default:
+                mismatch = $scope.edMismatchScore;
+                insert = $scope.edMismatchScore;
+                del = $scope.edMismatchScore;
+                match = $scope.edMatchScore;
+                break;
+
             }
-            
+
             console.log("run");
-            
+
             // run Needleman-Wunsch with provided params
             $scope.nw(mismatch, match, insert, del, $scope.subMatrix, $scope.scoringMethod);
+
+            // draw the traceback from the computed matrix
+            $scope.drawTraceback($scope.subMatrix);
         }
 
 
         $scope.nw = function (mismatch, match, insert, del, matrix, scoring) {
-            for(var i = 1; i < $scope.subMatrix.length; i++){
-                for(var j = 1; j < $scope.subMatrix[i].length; j++){
-                                        
-                    var up = $scope.subMatrix[i-1][j].value + insert;
-                    var left = $scope.subMatrix[i][j-1].value + del;
-                    var diag = ($scope.sequence1.charAt(j-1) === $scope.sequence2.charAt(i-1) ? $scope.subMatrix[i-1][j-1].value + match : $scope.subMatrix[i-1][j-1].value + mismatch);
+            for (var i = 1; i < matrix.length; i++) {
+                for (var j = 1; j < matrix[i].length; j++) {
+
+                    var up, left, diag;
                     
-                    console.log(up,left,diag);
-                    
-                    if(scoring === "editDistance"){
-                        if(up <= left && up <= diag){
-                            $scope.subMatrix[i][j].value = up;
-                            $scope.subMatrix[i][j].up = true;
+                    console.log($scope.sequence1.charAt(j - 1) + " vs " + $scope.sequence2.charAt(i - 1));
+
+
+                    // Diagonal scores are not constant
+                    if (scoring === "blosum45") {
+                        up = matrix[i][j-1].value + $scope.blossum45MismatchScore;
+                        left = matrix[i-1][j].value + $scope.blossum45MismatchScore;
+                        diag = matrix[i - 1][j - 1].value + $scope.blosum($scope.sequence1.charAt(i - 1), $scope.sequence2.charAt(j - 1));
+                    } else {
+                        up = matrix[i][j-1].value + insert;
+                        left = matrix[i-1][j].value + del;
+                        diag = ($scope.sequence1.charAt(i - 1) === $scope.sequence2.charAt(j - 1) ? matrix[i - 1][j - 1].value + match : matrix[i - 1][j - 1].value + mismatch);
+                    }
+
+                    // Looking for the min value
+                    if (scoring === "editDistance") {
+                        if (up <= left && up <= diag) {
+                            matrix[i][j].value = up;
+                            matrix[i][j].up = true;
                         }
-                        if(left <= up && left <= diag){
-                            $scope.subMatrix[i][j].value = left;
-                            $scope.subMatrix[i][j].left = true;
+                        if (left <= up && left <= diag) {
+                            matrix[i][j].value = left;
+                            matrix[i][j].left = true;
                         }
-                        if(diag <= up && diag <= left){
-                            $scope.subMatrix[i][j].value = diag;
-                            $scope.subMatrix[i][j].diag = true;
+                        if (diag <= up && diag <= left) {
+                            matrix[i][j].value = diag;
+                            matrix[i][j].diag = true;
                         }
                     }
+
                     // scoring is score or blossum
-                    else{
-                        if(up >= left && up >= diag){
-                            $scope.subMatrix[i][j].value = up;
-                            $scope.subMatrix[i][j].up = true;
+                    // Looking for max value
+                    else {
+                        console.log(up,left,diag);
+                        if (up >= left && up >= diag) {
+                            matrix[i][j].value = up;
+                            matrix[i][j].up = true;
                         }
-                        if(left >= up && left >= diag){
-                            $scope.subMatrix[i][j].value = left;
-                            $scope.subMatrix[i][j].left = true;
+                        if (left >= up && left >= diag) {
+                            matrix[i][j].value = left;
+                            matrix[i][j].left = true;
                         }
-                        if(diag >= up && diag >= left){
-                            $scope.subMatrix[i][j].value = diag;
-                            $scope.subMatrix[i][j].diag = true;
+                        if (diag >= up && diag >= left) {
+                            matrix[i][j].value = diag;
+                            matrix[i][j].diag = true;
                         }
                     }
-                    
                 }
             }
-            
-            
-
         }
-        
+
+
+        // input should be submatrix of scores
+        $scope.drawTraceback = function (matrix) {
+            var i = matrix.length - 1;
+            var j = matrix[i].length - 1;
+
+            console.log(i, j);
+            console.log(matrix[i][j]);
+
+            while (i >= 0 && j >= 0) {
+                console.log(i, j);
+
+                matrix[i][j].color = 'lightpink';
+
+                if (matrix[i][j].diag) {
+                    j--;
+                    i--;
+                } else if (matrix[i][j].left) {
+                    j--;
+                } else if (matrix[i][j].up) {
+                    i--;
+                } else if (i == 0 && j >= 0) {
+                    j--;
+                } else if (j == 0 && i >= 0) {
+                    i--;
+                }
+            }
+        }
+
         // shallow copy out score part of full matrix for use in NW
-        $scope.initSubmatrix = function(){
+        $scope.initSubmatrix = function () {
             // get just the score entries out of the full matrix
             $scope.subMatrix = $scope.matrix.slice(1, $scope.matrix.length);
             for (var i = 0; i < $scope.subMatrix.length; i++) {
